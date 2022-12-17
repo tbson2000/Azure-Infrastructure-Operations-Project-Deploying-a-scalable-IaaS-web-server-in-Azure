4b0aa6cd4ba5590b6ceb38fda47431065e0dace2
To BaoSon <to_baoson@krisshop.com>
2022-12-18 00:33:08 +0700

    combine to one main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,3 +87,88 @@ resource "azurerm_network_security_rule" "unsr_httpallowlb-vm" {
   resource_group_name         = azurerm_resource_group.urg.name
   network_security_group_name = azurerm_network_security_group.unsg.name
 }
+
+# Azure network
+
+resource "azurerm_network_interface" "unic" {
+  name                = "UdacityNIC"
+  location            = azurerm_resource_group.urg.location
+  resource_group_name = azurerm_resource_group.urg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.usubnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = var.required_tag
+}
+
+resource "azurerm_public_ip" "upip" {
+  name                = "UdacityPubIP"
+  resource_group_name = azurerm_resource_group.urg.name
+  location            = azurerm_resource_group.urg.location
+  allocation_method   = "Dynamic"
+
+  tags = var.required_tag
+}
+
+resource "azurerm_lb" "ulb" {
+  name                = "UdacityLB"
+  location            = azurerm_resource_group.urg.location
+  resource_group_name = azurerm_resource_group.urg.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.upip.id
+  }
+
+  tags = var.required_tag
+}
+
+resource "azurerm_lb_backend_address_pool" "ulb_bap" {
+  loadbalancer_id = azurerm_lb.ulb.id
+  name            = "UdacityLBBackendAddressPool"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "unibapa" {
+  network_interface_id    = azurerm_network_interface.unic.id
+  ip_configuration_name   = "PublicIPAddress"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.ulb_bap.id
+}
+
+# virtual machine
+
+resource "azurerm_availability_set" "uas" {
+  name                = "udacity-as"
+  location            = azurerm_resource_group.urg.location
+  resource_group_name = azurerm_resource_group.urg.name
+
+  tags = var.required_tag
+}
+
+resource "azurerm_linux_virtual_machine" "ulvm" {
+  name                            = format("udacity-machine-%d", count.index)
+  resource_group_name             = azurerm_resource_group.urg.name
+  location                        = azurerm_resource_group.urg.location
+  size                            = "Standard_DS2_V2"
+  admin_username                  = "admin"
+  admin_password                  = "Pa55w.rd"
+  availability_set_id             = azurerm_availability_set.uas.id
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.unic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_id = var.image_id
+
+  tags = var.required_tag
+
+  count = var.vm_num
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -11,7 +11,7 @@ variable "image_id" {
   description = "id of vm image from packer"
   type        = string
 
-  default = "/subscriptions/9264ae45-f6c6-47e0-9199-fa1b3e14415e/resourceGroups/ODL-clouddevops-218472/providers/Microsoft.Compute/images/myUbuntuPackerImage"
+  default = "/subscriptions/a4b11da3-2642-4ae2-b8e0-ba40545a13d6/resourceGroups/ODL-clouddevops-218998/providers/Microsoft.Compute/images/myUbuntuPackerImage"
 }
 
 variable "vm_num" {
